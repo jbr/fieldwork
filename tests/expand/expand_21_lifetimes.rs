a29838e0bc9178f416d8279df3236af7edb0badd
Jacob Rothstein <hi@jbr.me>
2025-07-10 14:31:59 -0700

    fix: option_set_some with borrows
    previously, option_set_some would erroneously strip references within the option, so for `#[field(set, option_set_some)] field: Option<&'a str>`, we would generate `set_field(&mut self, field: str) -> &mut Self`, but now we correctly generate `set_field(&mut self, field: &'a str) -> &mut Self`

--- tests/expand/expand_21_lifetimes.rs
+++ tests/expand/expand_21_lifetimes.rs
@@ -1,10 +1,12 @@
 use std::borrow::Cow;
 
 #[derive(fieldwork::Fieldwork)]
-#[fieldwork(get, get_mut)]
+#[fieldwork(get, get_mut, with, without, set(option_set_some))]
 struct MyStruct<'a> {
     borrow: &'a (),
     mut_borrow: &'a mut (),
+    #[field(into)]
     cow: Cow<'a, str>,
     box_dyn_trait: Box<dyn std::fmt::Debug + 'a>,
+    option_lifetime: Option<&'a str>,
 }
